ec0dc4452956d1a78472288052d8d4ad85264d39
adefamosaBYU <fam24002@byui.edu>
2025-03-08 22:15:16 +0100

    My first project using c#

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        // Generate a random magic number between 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+        int guess = 0;
+
+        Console.WriteLine("Welcome to Guess My Number!");
+
+        // Loop until the user guesses correctly
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess )
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+         List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        // Collect numbers from the user
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+        
+        // Compute the sum
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        
+        // Compute the average
+        double average = numbers.Count > 0 ? (double)sum / numbers.Count : 0;
+        Console.WriteLine($"The average is: {average}");
+        
+        // Find the maximum number
+        int maxNumber = numbers.Count > 0 ? numbers[0] : 0;
+        foreach (int num in numbers)
+        {
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxNumber}");
     }
 }
